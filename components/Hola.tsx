9c3f1dbe54f6d6d21cf28e74bc84496472aa5389
eloy garofano <eloygarov@gmail.com>
2025-11-24 10:18:56 +0100

    creo el componente que realiza la peticion (no terminado)

---
+++ components/Hola.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Post } from '../entities/Post';
+
+const Hola = () => {
+
+  const [saludo, setSaludo] = useState("Cargando...");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [datos, setDatos] = useState<Post[]>([]);
+
+  const saludo1 = () => {
+    return new Promise<number>( async (resolve, reject) => {
+      fetch("https://jsonplaceholder.typicode.com/posts").then(
+        (datos) => {
+          datos.json().then(
+            (datos: Post[]) => {
+              setDatos(datos);
+              setSaludo("Los datos se han leido correctamente");
+              setLoading(false);
+            }
+          )                    
+        }
+      )
+    })
+
+  }
+
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  )
+}
+
+export default Hola
+
+const styles = StyleSheet.create({})
